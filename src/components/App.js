08d151074430a3165f6ffa360b46d3bed4000a4a
Viraz <virajm72@gmail.com>
2020-06-06 23:05:37 +0530

    All Usecases Tested

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import logo from "../logo.png";
 import Web3 from "web3";
-import {executeLongEth, executeShortDai} from '../dsa/index'
+import {executeLongEth, executeShortDai, executeDebtSwap, executeLendingSwap} from "../dsa/index";
 import "./App.css";
 
 const DSA = require("dsa-sdk");
@@ -11,52 +11,41 @@ class App extends Component {
         super(props);
         this.state = { // UseCases That would be displayed to the user on the first screen
             usecases: [
-                'Long Eth',
-                'Short Dai',
-                'Debt Bridge',
-                'Lending Bridge',
-                'Debt Swap',
-                'Lending Swap'
+                "Long Eth", "Short Dai", "Debt Swap", "Lending Swap"
             ],
             // Usecase Mapping with the protocol options based on the usecase(NOTE -> We will include InstaPool as well for flash loans & so out of these the user can chose max upto two)
             // Additionaly before executing the transaction we will have to do extra validation for eg if user wants to go long on eth then compound is a mandatory protocol and he can choses either oasis/oneInch
             // NOTE -> For each Usecase it will be mandatory fot he user to choose exact 2
             useCaseProtocolObject: {
-                'Long Eth': [
-                    'oneInch', 'oasis', 'compound', 'aave'
+                "Long Eth": [
+                    "oneInch", "oasis", "compound", "aave"
                 ],
-                'Short Dai': [
-                    'oasis', 'maker', 'oneInch'
+                "Short Dai": [
+                    "oasis", "maker", "oneInch"
                 ],
-                'Debt Bridge': [
-                    'maker', 'compound', 'dydx', 'aave'
+                "Debt Swap": [
+                    "oasis", "compound", "oneInch", "aave"
                 ],
-                'Lending Bridge': [
-                    'maker', 'compound', 'dydx'
-                ],
-                'Debt Swap': [
-                    'oasis', 'compound', 'oneInch', 'aave'
-                ],
-                'Lending Swap': ['oasis', 'compound', 'oneInch']
+                "Lending Swap": ["oasis", "compound", "oneInch"]
             }
-        }
+        };
     }
 
     async componentWillMount() {
-        await this.loadWeb3()
-        await this.loadBlockchainData()
+        await this.loadWeb3();
+        await this.loadBlockchainData();
     }
 
     async loadWeb3() {
         if (window.ethereum) {
-            const web3 = new Web3(window.ethereum)
-            await window.ethereum.enable()
-            this.setState({web3})
+            const web3 = new Web3(window.ethereum);
+            await window.ethereum.enable();
+            this.setState({web3});
         } else if (window.web3) {
-            const web3 = new Web3(window.web3.currentProvider)
-            this.setState({web3})
+            const web3 = new Web3(window.web3.currentProvider);
+            this.setState({web3});
         } else {
-            window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+            window.alert("Non-Ethereum browser detected. You should consider trying MetaMask!");
         }
     }
 
@@ -72,31 +61,43 @@ class App extends Component {
         }
         existingDSAAddress = await dsa.getAccounts("0xf88b0247e611eE5af8Cf98f5303769Cba8e7177C");
         // Setting DSA Instance
-        await dsa.setInstance(existingDSAAddress[0].id)
-        this.setState({dsa})
+        await dsa.setInstance(existingDSAAddress[0].id);
+        this.setState({dsa});
         // For Testing only while integration this will be user's input
-        this.executeCustomisedTransaction("Short Dai", ["oasis", "maker"]);
-
+        this.executeCustomisedTransaction("Lending Swap", ["oneInch", "compound"]);
     }
-
 
     async executeCustomisedTransaction(usecase, protocols) {
         switch (usecase) {
             case "Long Eth":
-                if (!protocols.includes("oneInch") && !protocols.includes("oasis") || (protocols.includes("oneInch") && protocols.includes("oasis"))) 
+                if ((!protocols.includes("oneInch") && !protocols.includes("oasis")) || (protocols.includes("oneInch") && protocols.includes("oasis"))) 
                     throw new Error("Have to chosse either 1inch or oasis for Going Long on Eth");
-                 else if (!protocols.includes("aave") && !protocols.includes("compound") || (protocols.includes("aave") && protocols.includes("compound"))) 
+                 else if ((!protocols.includes("aave") && !protocols.includes("compound")) || (protocols.includes("aave") && protocols.includes("compound"))) 
                     throw new Error("Have to chosse either aave or compound for Going Long on Eth");
                  else 
                     return await executeLongEth(protocols, this.state.web3, this.state.dsa);
                 
 
             case "Short Dai":
-                if (!protocols.includes("oneInch") && !protocols.includes("oasis") || (protocols.includes("oneInch") && protocols.includes("oasis"))) 
+                if ((!protocols.includes("oneInch") && !protocols.includes("oasis")) || (protocols.includes("oneInch") && protocols.includes("oasis"))) 
                     throw new Error("Have to chosse either 1inch or oasis for Going Short on DAI");
                  else 
                     return await executeShortDai(protocols, this.state.web3, this.state.dsa);
                 
+
+            case "Debt Swap":
+                if ((!protocols.includes("oneInch") && !protocols.includes("oasis")) || (protocols.includes("oneInch") && protocols.includes("oasis"))) 
+                    throw new Error("Have to chosse either 1inch or oasis for Debt Swap");
+                 else if ((!protocols.includes("aave") && !protocols.includes("compound")) || (protocols.includes("aave") && protocols.includes("compound"))) 
+                    throw new Error("Have to chosse either aave or compound for Debt Swap");
+                 else 
+                    return await executeDebtSwap(protocols, this.state.web3, this.state.dsa);
+                
+             case "Lending Swap":
+                if ((!protocols.includes("oneInch") && !protocols.includes("oasis")) || (protocols.includes("oneInch") && protocols.includes("oasis"))) 
+                    throw new Error("Have to chosse either 1inch or oasis for Lending Swap");
+                 else 
+                    return await executeLendingSwap(protocols, this.state.web3, this.state.dsa);
 
             default:
                 throw new Error("Wrong Usecase Option");
--- src/dsa/index.js
+++ src/dsa/index.js
@@ -14,7 +14,6 @@ export const executeLongEth = async (protocols, web3, dsa) => {
         args: [dai_address, borrowAmtInWei, 0, 0]
     });
 
-
     if (protocols.includes("oasis")) {
         let buyDetail = await await dsa.oasis.getBuyAmount("ETH", "DAI", borrowAmount, slippage);
 
@@ -27,7 +26,7 @@ export const executeLongEth = async (protocols, web3, dsa) => {
                 borrowAmtInWei,
                 buyDetail.unitAmt,
                 0,
-                0,
+                0
             ]
         });
     } else { // Other option is oneInch
@@ -42,16 +41,16 @@ export const executeLongEth = async (protocols, web3, dsa) => {
                 borrowAmtInWei,
                 buyDetail.unitAmt,
                 0,
-                0,
+                0
             ]
         });
     }
-    if (protocols.includes('compound')) { // For Max Amt "-1" wasn't compatible
+    if (protocols.includes("compound")) { // For Max Amt "-1" wasn't compatible
         var BN = await web3.utils.BN;
         await spells.add({
             connector: "compound",
             method: "deposit",
-            args: [eth_address, new BN("115792089237316195423570985008687907853269984665640564039457584007913129639935"), 0, 0]
+            args: [eth_address, new BN("115792089237316195423570985008687907853269984665640564039457584007913129639935"), 0, 0,]
         });
 
         await spells.add({
@@ -64,7 +63,7 @@ export const executeLongEth = async (protocols, web3, dsa) => {
         await spells.add({
             connector: "aave",
             method: "deposit",
-            args: [eth_address, new BN("115792089237316195423570985008687907853269984665640564039457584007913129639935"), 0, 0]
+            args: [eth_address, new BN("115792089237316195423570985008687907853269984665640564039457584007913129639935"), 0, 0,]
         });
 
         await spells.add({
@@ -73,7 +72,6 @@ export const executeLongEth = async (protocols, web3, dsa) => {
             args: [dai_address, borrowAmtInWei, 0, 0]
         });
     }
-
 
     await spells.add({
         connector: "instapool",
@@ -86,20 +84,18 @@ export const executeLongEth = async (protocols, web3, dsa) => {
     };
 
     // For Simulation Testing on tenderly
-    var gasLimit = await await dsa.estimateCastGas(data).catch(err => {
+    var gasLimit = await await dsa.estimateCastGas(data).catch((err) => {
         return console.log(err);
     });
-}
+};
 
 export const executeShortDai = async (protocols, web3, dsa) => {
-
     let borrowAmount = 20; // 20 DAI
     let borrowAmtInWei = await dsa.tokens.fromDecimal(borrowAmount, "dai"); // borrow flash loan and swap via Oasis
 
     let slippage = 2; // 2% slippage.
-    let dai_address = await dsa.tokens.info.dai.address
-    let usdc_address = await dsa.tokens.info.usdc.address
-
+    let dai_address = await dsa.tokens.info.dai.address;
+    let usdc_address = await dsa.tokens.info.usdc.address;
 
     let spells = await dsa.Spell();
 
@@ -120,7 +116,7 @@ export const executeShortDai = async (protocols, web3, dsa) => {
                 borrowAmtInWei,
                 buyAmount.unitAmt,
                 0,
-                0
+                0,
             ]
         });
     } else {
@@ -135,11 +131,10 @@ export const executeShortDai = async (protocols, web3, dsa) => {
                 borrowAmtInWei,
                 buyAmount.unitAmt,
                 0,
-                0
+                0,
             ]
         });
     }
-
 
     await spells.add({connector: "maker", method: "open", args: ["USDC-A"]});
     var BN = await web3.utils.BN;
@@ -147,7 +142,9 @@ export const executeShortDai = async (protocols, web3, dsa) => {
     await spells.add({
         connector: "maker",
         method: "deposit",
-        args: [0, new BN("115792089237316195423570985008687907853269984665640564039457584007913129639935"), 0, 0] // deposit all USDC
+        args: [
+            0, new BN("115792089237316195423570985008687907853269984665640564039457584007913129639935"), 0, 0,
+        ], // deposit all USDC
     });
 
     await spells.add({
@@ -166,7 +163,171 @@ export const executeShortDai = async (protocols, web3, dsa) => {
         spells: spells
     };
     // For Simulation Testing on tenderly
-    var gasLimit = await dsa.estimateCastGas(data).catch(err => {
+    var gasLimit = await dsa.estimateCastGas(data).catch((err) => {
+        return console.log(err);
+    });
+};
+
+export const executeDebtSwap = async (protocols, web3, dsa) => {
+    let borrowAmount = 20; // 20 DAI
+    let borrowAmtInWei = await dsa.tokens.fromDecimal(borrowAmount, "dai"); // borrow flash loan and swap via Oasis
+
+    let slippage = 2; // 0.1% slippage.
+    let dai_address = await dsa.tokens.info.dai.address;
+    let usdc_address = await dsa.tokens.info.usdc.address;
+
+    let spells = await dsa.Spell();
+
+    await spells.add({
+        connector: "instapool",
+        method: "flashBorrow",
+        args: [dai_address, borrowAmtInWei, 0, 0]
+    });
+    if (protocols.includes("oasis")) {
+        let buyAmount = await dsa.oasis.getBuyAmount("USDC", "DAI", borrowAmount, slippage);
+
+        await spells.add({
+            connector: "oasis",
+            method: "sell",
+            args: [
+                usdc_address,
+                dai_address,
+                borrowAmtInWei,
+                buyAmount.unitAmt,
+                0,
+                0,
+            ]
+        });
+    } else {
+        let buyAmount = await dsa.oneInch.getBuyAmount("USDC", "DAI", borrowAmount, slippage);
+
+        await spells.add({
+            connector: "oneInch",
+            method: "sell",
+            args: [
+                usdc_address,
+                dai_address,
+                borrowAmtInWei,
+                buyAmount.unitAmt,
+                0,
+                0,
+            ]
+        });
+    }
+
+    if (protocols.includes("compound")) {
+        var BN = await web3.utils.BN;
+
+        await spells.add({
+            connector: "compound",
+            method: "payback",
+            args: [usdc_address, new BN("115792089237316195423570985008687907853269984665640564039457584007913129639935"), 0, 0]
+        });
+
+        await spells.add({
+            connector: "compound",
+            method: "borrow",
+            args: [dai_address, borrowAmount, 0, 0]
+        });
+    } else {
+        var BN = await web3.utils.BN;
+        await spells.add({
+            connector: "aave",
+            method: "payback",
+            args: [
+                usdc_address, new BN("115792089237316195423570985008687907853269984665640564039457584007913129639935"), 0, 0
+            ], // get Payback amount with id 423
+        });
+
+        await spells.add({
+            connector: "aave",
+            method: "borrow",
+            args: [dai_address, borrowAmount, 0, 0]
+        });
+    }
+
+    await spells.add({
+        connector: "instapool",
+        method: "flashPayback",
+        args: [dai_address, 0, 0]
+    });
+
+    var data = {
+        spells: spells
+    };
+
+    // For Simulation Testing on tenderly
+    var gasLimit = await dsa.estimateCastGas(data).catch((err) => {
+        return console.log(err);
+    });
+};
+
+export const executeLendingSwap = async (protocols, web3, dsa) => {
+    let withdrawAmount = 20; // 20 DAI
+    let withdrawAmtInWei = dsa.tokens.fromDecimal(withdrawAmount, "dai"); // borrow flash loan and swap via Oasis
+
+    let slippage = 2; // 0.1% slippage.
+    let dai_address = await dsa.tokens.info.dai.address
+    let usdc_address = await dsa.tokens.info.usdc.address
+
+    let buyAmount = await dsa.oasis.getBuyAmount("USDC", "DAI", withdrawAmount, slippage);
+
+    let spells = await dsa.Spell();
+
+    await spells.add({
+        connector: "compound",
+        method: "withdraw",
+        args: [dai_address, withdrawAmtInWei, 0, 0]
+    });
+
+    if (protocols.includes("oasis")) {
+        let buyAmount = await dsa.oasis.getBuyAmount("USDC", "DAI", withdrawAmount, slippage);
+
+        await spells.add({
+            connector: "oasis",
+            method: "sell",
+            args: [
+                usdc_address,
+                dai_address,
+                withdrawAmtInWei,
+                buyAmount.unitAmt,
+                0,
+                0
+            ] // setting USDC amount with id 423
+        });
+
+    } else {
+        let buyAmount = await dsa.oneInch.getBuyAmount("USDC", "DAI", withdrawAmount, slippage);
+
+        await spells.add({
+            connector: "oneInch",
+            method: "sell",
+            args: [
+                usdc_address,
+                dai_address,
+                withdrawAmtInWei,
+                buyAmount.unitAmt,
+                0,
+                0
+            ] // setting USDC amount with id 423
+        });
+    }
+
+
+    var BN = await web3.utils.BN;
+
+    await spells.add({
+        connector: "compound",
+        method: "deposit",
+        args: [usdc_address, new BN("115792089237316195423570985008687907853269984665640564039457584007913129639935"), 0, 0]
+    });
+
+    var data = {
+        spells: spells
+    };
+
+    // For Simulation Testing on tenderly
+    var gasLimit = await dsa.estimateCastGas(data).catch((err) => {
         return console.log(err);
     });
 }
