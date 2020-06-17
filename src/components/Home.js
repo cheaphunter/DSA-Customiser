bd0e207453fa5007c0e1519442be4a5d37cc9bc5
snaketh4xor <snaaketh4xor@protonmail.com>
2020-06-17 14:55:18 +0530

    Home styled complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import "./Home.css";
 
 class Home extends Component {
 
   render() {
     return (
-      <div>
+      <div id="container">
+	  <div className="content">
       <h1>DSA</h1>
       <h3>easy to use custom defi dashboard</h3>
-<Link to='/custom'><button type="submit" className="button">Try Customiser</button></Link>
+<Link to='/custom'><button type="submit" id="btn">Try Customiser</button></Link>
+</div>
 </div>
     );
   }
